9e7aff416dfc0854a0ec3d897f2a68171c780b3a
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-08-26 06:18:37 +0330

    adding contact && testiomonials

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,72 @@
+import { contactOptions } from "../../sources";
 import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="Contact">
+      <div className="wrapper">
+        <div className="contact-options">
+          {contactOptions.map((option, index) => (
+            <div className="flex-center option" key={index}>
+              <div className="flex-center icon-wrapper ">{option.icon}</div>
+              <h4 className="muted">{option.title}</h4>
+              <h3 className="muted">{option.value}</h3>
+            </div>
+          ))}
+        </div>
+        <div className="contact-form">
+          <div className="top">
+            <h1 className="title">
+              <span className="gradient-text"> زود به ما ملحق شو!</span>
+            </h1>
+            <p className="muted">
+              من با صبر و حوصله خیلی زیاد نقاشی میکنم چون خیلی دوس دارم کارمو
+            </p>
+          </div>
+          <div className="middle">
+            <div className="flex row">
+              <input
+                type="text"
+                placeholder="اسم"
+                name="firstname"
+                className="control"
+              />{" "}
+              <input
+                type="text"
+                placeholder="فامیلی"
+                name="LastName"
+                className="control"
+              />
+            </div>
+            <div className="flex row">
+              <input
+                type="email"
+                placeholder="ایمیل"
+                name="email"
+                className="control"
+              />{" "}
+              <input
+                type="tel"
+                placeholder="+98"
+                name="tel"
+                className="control"
+              />
+            </div>
+            <textarea
+              name="message"
+              cols={30}
+              rows={10}
+              placeholder="پیام زیباتون!"
+              className="control"
+            ></textarea>
+          </div>
+          <div className="bottom flex-center">
+            <button className="btn primary">ارسال کنید!</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,77 @@
+import Slider from "react-slick";
 import "./Testimonials.css";
-
+import { clients } from "../../sources";
+import { FaStar } from "react-icons/fa6";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useRef } from "react";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 const Testimonials = () => {
+  const ref = useRef(null);
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    puseOnHover: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    response: [
+      {
+        breakpoint: 900,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="Testimonials">
+      <div className="wrapper">
+        <div className="section-header">
+          <h1 className="heading-1">
+            <span className="gradient-text">گواهینامه ها</span>
+          </h1>
+          <h4 className="sub-title muted">کارفرما های من چی میگن؟</h4>
+        </div>
+        <Slider className="Testimonials-container" {...settings} ref={ref}>
+          {clients.map((list, index) => (
+            <>
+              <div className="flex" key={index}>
+                <div className="details">
+                  <h3 className="name">{list.name}</h3>
+                  <small className="muted">مدیر اجرایی شرکت دیجیکالا</small>
+                </div>
+                <div className="profile">
+                  <img src={list.image} alt={list.name} />
+                </div>
+              </div>
+              <p className="p muted content">{list.review}</p>
+              <div className="stars-container">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+            </>
+          ))}
+        </Slider>
 
-export default Testimonials
+        <div className="flex-center buttons-container">
+          <button
+            className="flex-center btn"
+            onClick={() => ref.current.slickNext()}
+          >
+            <FaLongArrowAltRight />
+          </button>
+          <button
+            className="flex-center btn"
+            onClick={() => ref.current.slickPrev()}
+          >
+            <FaLongArrowAltLeft />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
